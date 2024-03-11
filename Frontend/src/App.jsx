9e85cb29f85056c3569daf5e56e803ef18fa4f00
Tarun Koshti <tarunkoshti910@gmail.com>
2024-03-11 19:05:27 +0530

    input and button component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import Signup from './Components/Signup'
 
 const App = () => {
-  // const [data, setData] = useState({})
-  // axios.get("/api/user/")
-  //   .then((res) => setData(res.data))
-  
+
   return (
     <div>
-      Internshala
-      <p className='font-bold'>{data.message}</p>
+      {/* Internshala */}
+      <Signup />
     </div>
   )
 }
