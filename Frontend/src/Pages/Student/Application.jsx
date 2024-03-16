ebbe22eb9e0ee425fecb96de3999f6c98e1c2689
Prashantpatel698 <prashantkirar698@gmail.com>
2024-03-16 21:43:39 +0530

    Applicaton issue solve by prashnat

--- Frontend/src/Pages/Student/Application.jsx
+++ Frontend/src/Pages/Student/Application.jsx
@@ -6,23 +6,22 @@ import { allapplyinternship, currentUser } from '../../store/Actions/userActions
 const Application = () => {
 
   const dispatch = useDispatch()
-  const appliedinternships = useSelector((state) => state.internshipReducer.internshipData)
+  const appliedinternships = useSelector((state) => state.internshipReducer.internshipData.internships)
+  console.log(appliedinternships)
   const { student } = useSelector((state) => state.userReducer.userData)
 
 
   useEffect(() => {
-
     dispatch(currentUser())
     dispatch(allapplyinternship(student._id))
   }, [dispatch])
-  const intern = appliedinternships.internships
-  console.log(intern)
+  
   return (
     <div className='bg-gray-300 mx-20'>
       <h1 className='text-3xl text-center font-semibold'>My Application</h1>
 
       <ul>
-        {intern.map((i, index) => (
+        {appliedinternships && appliedinternships.map((i, index) => (
           <div className='px-10 bg-blue-200 my-2'>
             <h1>{index + 1}</h1><h1>{i.company}</h1>
           </div>
