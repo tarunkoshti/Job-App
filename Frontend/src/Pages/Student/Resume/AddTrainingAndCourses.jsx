f7156ebf7357374392ed97c7424dd2bf368308d9
Tarun Koshti <tarunkoshti910@gmail.com>
2024-04-19 21:37:51 +0530

    resume course from validation done

--- Frontend/src/Pages/Student/Resume/AddTrainingAndCourses.jsx
+++ Frontend/src/Pages/Student/Resume/AddTrainingAndCourses.jsx
@@ -3,10 +3,13 @@ import Input from '../../../Components/Input'
 import { useDispatch, useSelector } from 'react-redux'
 import { useForm } from 'react-hook-form'
 import Button from '../../../Components/Button'
-import { addInternship, addJob, addTrainingCourse, editTrainingCourse } from '../../../store/Actions/userActions'
-import { Link, useNavigate, useParams } from 'react-router-dom'
+import { addTrainingCourse, editTrainingCourse } from '../../../store/Actions/userActions'
+import { useNavigate, useParams } from 'react-router-dom'
 import { RxCross2 } from "react-icons/rx";
-
+import { toast } from 'react-toastify'
+import { MdErrorOutline } from 'react-icons/md'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const AddTrainingAndCourses = ({ edit = false }) => {
 
@@ -16,14 +19,23 @@ const AddTrainingAndCourses = ({ edit = false }) => {
 
   let [currlength, setCurrlength] = useState(0)
 
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit, watch, formState: { errors }, setValue } = useForm();
   const dispatch = useDispatch();
   const navigate = useNavigate()
 
   const submit = async (data) => {
     if (currlength <= 500) {
-      edit ? await dispatch(editTrainingCourse(id, student._id, data))
-        : await dispatch(addTrainingCourse(student._id, data))
+      if (edit) {
+        const error = await dispatch(editTrainingCourse(id, student._id, data))
+        error ? toast.error(error.data.message)
+          : toast.success("Training/Course updated")
+      } else {
+        const error = await dispatch(addTrainingCourse(student._id, data))
+        error ? toast.error(error.data.message)
+          : toast.success("Training/Course added")
+      }
+      // edit ? await dispatch(editTrainingCourse(id, student._id, data))
+      //   : await dispatch(addTrainingCourse(student._id, data))
       navigate("/student/resume")
     }
   }
@@ -35,14 +47,22 @@ const AddTrainingAndCourses = ({ edit = false }) => {
   useEffect(() => {
     const descriptionValue = watch((value, { name }) => {
       if (name == "description") {
-        let str = value.description
+        let str = value.description.trim("/n")
         setCurrlength(str.length)
+      }
+      if (name == "workType") {
+        value.workType ? setValue("Location", "Online")
+          : setValue("Location", "")
+      }
+      if (name == "currentWorking") {
+        value.currentWorking ? setValue("endDate", "Currently Working")
+          : setValue("endDate", "")
       }
     });
 
   }, [watch]);
 
-  const course = student?.resume?.courses.filter(item => item.id === id)
+  const course = student?.resume?.courses.find(item => item.id === id)
 
   return (
     < div className='w-full h-screen absolute top-[0]' >
@@ -55,78 +75,124 @@ const AddTrainingAndCourses = ({ edit = false }) => {
           className='w-full p-10 flex flex-col gap-5'>
           <h1 className='text-center text-xl font-semibold'>Training details</h1>
 
-          <Input
-            defaultValue={edit ? (course?.training || '') : ''}
-            label="Training program"
-            placeholder="e.g. Analytics"
-            {...register("training", {
-              required: true
-            })}
-          />
-          <Input
-            defaultValue={edit ? (course?.organization || '') : ''}
-            label="Organization"
-            placeholder="e.g. Career Training"
-            {...register("organization", {
-              required: true
-            })}
-          />
-          <Input
-            defaultValue={edit ? (course?.Location || '') : ''}
-            label="Location"
-            placeholder="e.g. Mumbai"
-            {...register("Location", {
-              // required: true
-            })}
-          />
-          <div className='w-full flex gap-2'>
-
+          <div>
             <Input
-              defaultValue={edit ? (course?.startDate || '') : ''}
-              type="date"
-              label="Start date"
-              placeholder="Choose date"
-              {...register("startDate", {
+              defaultValue={edit ? (course?.training || '') : ''}
+              label="Training program"
+              placeholder="e.g. Analytics"
+              {...register("training", {
+                required: {
+                  value: true,
+                  message: "training is required"
+                },
               })}
             />
-            <Input
-              defaultValue={edit ? (course?.endDate || '') : ''}
-              type="date"
-              label="End date"
-              placeholder="Choose date"
-              {...register("endDate", {
-              })}
-            />
-
+            {errors.training && <p className="text-red-500 text-sm mt-1 flex items-center gap-1"><MdErrorOutline /> <span>{errors.training.message}</span></p>}
           </div>
 
-          <div className='w-full flex gap-2 '>
+          <div>
+            <Input
+              defaultValue={edit ? (course?.organization || '') : ''}
+              label="Organization"
+              placeholder="e.g. Career Training"
+              {...register("organization", {
+                required: {
+                  value: true,
+                  message: "organization name is required"
+                },
+              })}
+            />
+            {errors.organization && <p className="text-red-500 text-sm mt-1 flex items-center gap-1"><MdErrorOutline /> <span>{errors.organization.message}</span></p>}
+          </div>
 
-            <label className='w-1/2 pl-1 flex gap-1.5 items-center'>
+          <div>
+            <Input
+              defaultValue={edit ? (course?.Location || '') : ''}
+              label="Location"
+              placeholder="e.g. Mumbai"
+              {...register("Location", {
+                // required: true
+              })}
+              readOnly={watch("workType")}
+            />
+            <label className='w-full pt-1 pl-1 flex gap-1.5 items-center'>
               <input
                 defaultChecked={edit ? (course?.workType || '') : ''}
                 type="checkbox"
                 {...register("workType", {
                 })}
               />
-              <span>Online</span>
+              <span className='text-sm font-semibold'>Online</span>
             </label>
+          </div>
 
-            <label className='w-1/2 pl-1 flex gap-1.5 items-center'>
-              <input
-                defaultChecked={edit ? (course?.currentWorking || '') : ''}
-                type="checkbox"
-                {...register("currentWorking", {
+          <div className='w-full flex gap-2'>
+
+            <div>
+              <Input
+                defaultValue={edit ? (course?.startDate || '') : ''}
+                type="text"
+                label="Start date"
+                placeholder="Choose date"
+                {...register("startDate", {
+                  required: {
+                    value: true,
+                    message: "required"
+                  }
                 })}
-              />
-              <span>Currently ongoing</span>
-            </label>
+              >
+                <DatePicker
+                  className='w-full outline-none px-3 py-2 bg-red-500 rounded-lg opacity-0'
+                  onChange={(date) => {
+                    const formattedDate = date.toLocaleDateString('en-GB'); // Format: dd/MM/yyyy
+                    setValue("startDate", formattedDate);
+                  }}
+                />
+              </Input>
+              {errors.startDate && <p className="text-red-500 text-sm mt-1 flex items-center gap-1"><MdErrorOutline /> <span>{errors.startDate.message}</span></p>}
+            </div>
+
+            <div>
+              <Input
+                defaultValue={edit ? (course?.endDate || '') : ''}
+                type="text"
+                label="End date"
+                placeholder="Choose date"
+                {...register("endDate", {
+                  required: {
+                    value: true,
+                    message: "required"
+                  }
+                })}
+              >
+                <DatePicker
+                  className='w-full outline-none px-3 py-2 bg-red-500 rounded-lg opacity-0'
+                  onChange={(date) => {
+                    const formattedDate = date.toLocaleDateString('en-GB'); // Format: dd/MM/yyyy
+                    setValue("endDate", formattedDate);
+                  }}
+                  readOnly={watch("currentWorking")}
+                />
+              </Input>
+
+              {errors.endDate && <p className="text-red-500 text-sm mt-1 flex items-center gap-1"><MdErrorOutline /> <span>{errors.endDate.message}</span></p>}
+
+              <label className='w-full pt-1 pl-1 flex gap-1.5 items-center'>
+                <input
+                  defaultChecked={edit ? (course?.currentWorking || '') : ''}
+                  type="checkbox"
+                  {...register("currentWorking", {
+                  })}
+                />
+                <span className='text-sm pt-1 font-semibold'>Currently ongoing</span>
+              </label>
+            </div>
 
           </div>
 
           <label htmlFor="des" className='flex flex-col gap-1'>
-            <span>Description (Optional)</span>
-            {errors.description && <p className="text-red-500 text-sm">{errors.description.message}</p>}
+            <span>Description (optional)</span>
+            {errors.description && <p className="text-red-500 text-sm mt-1 flex items-center gap-1"><MdErrorOutline /> <span>{errors.description.message}</span></p>}
             <textarea
               defaultValue={edit ? (course?.description || '') : ''}
               name="description"
@@ -138,6 +204,16 @@ const AddTrainingAndCourses = ({ edit = false }) => {
                 maxLength: {
                   value: 500,
                   message: "Description should not exceed 500 characters."
+                },
+                validate: {
+                  bulletPoints: value => {
+                    if (!value || /^\s*$/.test(value)) {
+                      return true;
+                    }
+                    const bulletPoints = value.split('\n');
+                    return bulletPoints.every(point => /^\s*\d+\.\s*/.test(point.trim()))
+                      || "Each point must start with a number followed by a dot.";
+                  },
                 }
               })}
             />
