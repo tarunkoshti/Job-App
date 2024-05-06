ea812e1dd2edb7aada79226f3fefcd9396fb6c87
IdeaProjects <harshitchourasiya0808@gmail.com>
2024-05-07 00:41:19 +0530

    Few Changes

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -13,65 +13,60 @@ const Footer = () => {
     const authStatus = isStudentAuth || isEmployeeAuth
 
     return (
-
-        <div className="py-8 mt-10 bg-zinc-100">
-            <div className="flex flex-col md:flex-row items-center justify-between px-4 md:px-8 lg:px-32">
-                <img className="w-24 md:w-32" src="logo1.jpg" alt="Footer" />
-                <div className="flex items-center gap-5 mt-4 md:mt-0">
-                    <FaInstagram className="text-2xl md:text-3xl" />
-                    <FaFacebook className="text-2xl md:text-3xl" />
-                    <FaLinkedinIn className="text-2xl md:text-3xl" />
-                    <RiTwitterXFill className="text-2xl md:text-3xl" />
-                    <FaYoutube className="text-2xl md:text-3xl" />
-                </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-6 md:gap-32 px-4 md:px-8 lg:px-32 py-10">
-                <div className="flex flex-col gap-2">
-                    <h6 className="font-semibold text-lg">Company</h6>
-                    <br />
-                    <h6>About Us</h6>
-                    <h6>Careers</h6>
-                    <h6>Contact Us</h6>
-                    <h6>Blog</h6>
-                </div>
-                <div className="flex flex-col gap-3">
-                    <h6 className="font-semibold text-lg">Resource</h6>
-                    <br />
-                    <h6>Help Center</h6>
-                    <h6>How It Works</h6>
-                    <h6>Business</h6>
-                    <h6>Pages</h6>
-                </div>
-                <div className="flex flex-col gap-3">
-                    <h6 className="font-semibold text-lg">Projects</h6>
-                    <br />
-                    <h6>Design & Multimedia</h6>
-                    <h6>IT & Programming</h6>
-                    <h6>Sales & Marketing</h6>
-                    <h6>Writing & Translating</h6>
-                </div>
-                <div className="flex flex-col gap-3">
-                    <h6 className="font-semibold text-lg">Freelancer</h6>
-                    <br />
-                    <h6>Design</h6>
-                    <h6>Marketing</h6>
-                    <h6>Engineering</h6>
-                    <h6>Animator</h6>
-                </div>
-            </div>
-
-            <div className="flex flex-col md:flex-row items-center justify-center md:justify-around font-semibold  md:px-32 lg:px-0 py-4 md:py-8">
-                <h6 className="mb-2 md:mb-0">Job Seekers 2024</h6>
-                <h6 className="mb-2 md:mb-0">Terms & Conditions</h6>
-                <h6 className="mb-2 md:mb-0">Privacy Policy</h6>
-                <h6 className="mb-2 md:mb-0">Accessibility Statement</h6>
-            </div>
-
+      <div className="py-8 mt-10 bg-gray-50">
+        <div className="flex flex-col md:flex-row items-center justify-between px-4 md:px-8 lg:px-32">
+          <img className="w-24 md:w-32" src="Job Seeking2.png" alt="Footer" />
+          <div className="flex items-center gap-5 mt-4 md:mt-0">
+            <FaInstagram className="text-2xl md:text-3xl" />
+            <FaFacebook className="text-2xl md:text-3xl" />
+            <FaLinkedinIn className="text-2xl md:text-3xl" />
+            <RiTwitterXFill className="text-2xl md:text-3xl" />
+            <FaYoutube className="text-2xl md:text-3xl" />
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-6 md:gap-32 px-4 md:px-8 lg:px-32 py-10">
+          <div className="flex flex-col gap-2">
+            <h6 className="font-semibold text-lg">Company</h6>
+            <br />
+            <h6>About Us</h6>
+            <h6>Careers</h6>
+            <h6>Contact Us</h6>
+            <h6>Blog</h6>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h6 className="font-semibold text-lg">Resource</h6>
+            <br />
+            <h6>Help Center</h6>
+            <h6>How It Works</h6>
+            <h6>Business</h6>
+            <h6>Pages</h6>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h6 className="font-semibold text-lg">Projects</h6>
+            <br />
+            <h6>Design & Multimedia</h6>
+            <h6>IT & Programming</h6>
+            <h6>Sales & Marketing</h6>
+            <h6>Writing & Translating</h6>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h6 className="font-semibold text-lg">Freelancer</h6>
+            <br />
+            <h6>Design</h6>
+            <h6>Marketing</h6>
+            <h6>Engineering</h6>
+            <h6>Animator</h6>
+          </div>
         </div>
 
-
-
-    )
+        <div className="flex flex-col md:flex-row items-center justify-center md:justify-around font-semibold  md:px-32 lg:px-0 py-4 md:py-8">
+          <h6 className="mb-2 md:mb-0">Job Seekers 2024</h6>
+          <h6 className="mb-2 md:mb-0">Terms & Conditions</h6>
+          <h6 className="mb-2 md:mb-0">Privacy Policy</h6>
+          <h6 className="mb-2 md:mb-0">Accessibility Statement</h6>
+        </div>
+      </div>
+    );
 
 }
 
--- Frontend/src/Components/Header/EmployeHader.jsx
+++ Frontend/src/Components/Header/EmployeHader.jsx
@@ -36,70 +36,146 @@ const EmployeeHeader = () => {
     };
 
     return (
-        <header className='w-full  bg-white text-gray-500 font-semibold'>
-            <nav className='w-full px-10 py-5 flex justify-between items-center relative border-b-2 pb-3 '>
-                {/* leftItem */}
-                <div className='w-[200px]'>
-                    <Link to="/employee">
-                        <img className='max-sm:h-6 h-10' src="Job Seeking2.png" alt="./logo.jpg" />
-                    </Link>
-                </div>
-                {/* middleItems */}
-                <ul className='hidden lg:flex justify-center gap-12 items-center'>
-                    <li>
-                        <Link to="/employee/internships" className='text-black font-normal text-lg hover:text-[#2507B3]'>
-                            Internships
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/employee/jobs" className='text-black font-normal text-lg hover:text-[#2507B3]'>
-                            Jobs
-                        </Link>
-                    </li>
-                </ul>
-                {/* rightItems */}
-                <div className='w-[200px] flex justify-end gap-8 items-center'>
-                    <div className='h-10 w-10 border-2 rounded-full flex items-center justify-center cursor-pointer' onClick={toggleProfile}>
-                        <img className='rounded-full h-full w-full' src={employe.avatar.url} alt="" />
-                    </div>
-                </div>
-            </nav>
-            {/*  profile options */}
-            <div className={isProfileOpen ? `overlay w-full h-screen fixed top-0 bg-black opacity-50` : "hidden"} onClick={backHandler}></div>
-            <div className={`h-full w-full max-w-sm fixed top-0 right-0 bg-gray-100 p-4 ${isProfileOpen ? "animate__slideInRight" : "animate__slideOutRight"}`}>
-                <div className='w-full flex justify-end px-10 py-6'>
-                    <RxCross2 onClick={backHandler} size={27} className='cursor-pointer text-black ' />
-                </div>
-                <div className='w-full flex flex-col items-center border-b py-2 mb-4'>
-                    <div className='h-12 w-12 rounded-full border-2 relative'>
-                        <img className='rounded-full h-full w-full' src={employe.avatar.url} alt="" />
-                        <AiOutlineEdit size={27} className='absolute bottom-0 -right-8 cursor-pointer hover:bg-gray-200 rounded-full p-1.5 text-black' />
-                    </div>
-                    <h6 className='text-black capitalize font-semibold text-sm'>{employe.firstname} {employe.lastname}</h6>
-                    <span className='font-normal text-sm'>{employe.email}</span>
-                </div>
-                <div className='w-full flex flex-col gap-3 px-2 text-black text-lg font-normal'>
-                    <div className='w-full flex flex-col gap-3'>
-                        <Link to="/employee" onClick={backHandler} className='hover:text-blue-600'>Home</Link>
-                        <Link to="/employee/internships/create" onClick={backHandler} className='hover:text-blue-600'>Create Internship</Link>
-                        <Link to="/employee/jobs/create" onClick={backHandler} className='hover:text-blue-600'>Create Job</Link>
-                    </div>
-                    <div className='w-full'>
-                        <button className='w-full flex justify-between items-center mb-3 hover:text-blue-600' onClick={Dropdown}>
-                            <span>Manage Account</span>
-                            <RiArrowDropDownLine size={25} />
-                        </button>
-                        {dropdown && (
-                            <div className='w-5/6 ml-auto flex flex-col gap-3'>
-                                <Link to="/employee/reset-password" onClick={backHandler} className='hover:text-blue-600'>Change Password</Link>
-                                <Link to="/employee/forget-password" onClick={backHandler} className='hover:text-blue-600'>Forget Password</Link>
-                                <Link onClick={LogoutHandler} className='hover:text-blue-600'>Logout</Link>
-                            </div>
-                        )}
-                    </div>
-                </div>
+      <header className="w-full  bg-white text-gray-500 font-semibold">
+        <nav className="w-full px-10 py-5 flex justify-between items-center relative border-b-2 pb-3 ">
+          {/* leftItem */}
+          <div className="w-[200px]">
+            <Link to="/employee">
+              <img
+                className="max-sm:h-6 h-10"
+                src="Job Seeking2.png"
+                alt="./logo.jpg"
+              />
+            </Link>
+          </div>
+          {/* middleItems */}
+          <ul className="hidden lg:flex justify-center gap-12 items-center">
+            <li>
+              <Link
+                to="/employee/internships"
+                className="text-black font-normal text-lg hover:text-[#2507B3]"
+              >
+                Internships
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/employee/jobs"
+                className="text-black font-normal text-lg hover:text-[#2507B3]"
+              >
+                Jobs
+              </Link>
+            </li>
+          </ul>
+          {/* rightItems */}
+          <div className="w-[200px] flex justify-end gap-8 items-center">
+            <div
+              className="h-10 w-10 border-2 rounded-full flex items-center justify-center cursor-pointer"
+              onClick={toggleProfile}
+            >
+              <img
+                className="rounded-full h-full w-full"
+                src={employe.avatar.url}
+                alt=""
+              />
             </div>
-        </header>
+          </div>
+        </nav>
+        {/*  profile options */}
+        <div
+          className={
+            isProfileOpen
+              ? `overlay w-full h-screen fixed top-0 bg-black opacity-50`
+              : "hidden"
+          }
+          onClick={backHandler}
+        ></div>
+        <div
+          className={`h-full w-full max-w-sm fixed top-0 right-0 bg-gray-100 p-4 ${
+            isProfileOpen ? "animate__slideInRight" : "animate__slideOutRight"
+          }`}
+        >
+          <div className="w-full flex justify-end px-10 py-6">
+            <RxCross2
+              onClick={backHandler}
+              size={27}
+              className="cursor-pointer text-black "
+            />
+          </div>
+          <div className="w-full flex flex-col items-center border-b py-2 mb-4">
+            <div className="h-12 w-12 rounded-full border-2 relative">
+              <img
+                className="rounded-full h-full w-full"
+                src={employe.avatar.url}
+                alt=""
+              />
+              <AiOutlineEdit
+                size={27}
+                className="absolute bottom-0 -right-8 cursor-pointer hover:bg-gray-200 rounded-full p-1.5 text-black"
+              />
+            </div>
+            <h6 className="text-black capitalize font-semibold text-sm">
+              {employe.firstname} {employe.lastname}
+            </h6>
+            <span className="font-normal text-sm">{employe.email}</span>
+          </div>
+          <div className="w-full flex flex-col gap-3 px-2 text-black text-lg font-normal">
+            <div className="w-full flex flex-col gap-3">
+              <Link
+                to="/employee"
+                onClick={backHandler}
+                className="hover:text-blue-600"
+              >
+                Home
+              </Link>
+              <Link
+                to="/employee/internships/create"
+                onClick={backHandler}
+                className="hover:text-blue-600"
+              >
+                Create Internship
+              </Link>
+              <Link
+                to="/employee/jobs/create"
+                onClick={backHandler}
+                className="hover:text-blue-600"
+              >
+                Create Job
+              </Link>
+            </div>
+            <div className="w-full">
+              <button
+                className="w-full flex justify-between items-center mb-3 hover:text-blue-600"
+                onClick={Dropdown}
+              >
+                <span>Manage Account</span>
+                <RiArrowDropDownLine size={25} />
+              </button>
+              {dropdown && (
+                <div className="w-5/6 ml-auto flex flex-col gap-3">
+                  <Link
+                    to="/employee/reset-password"
+                    onClick={backHandler}
+                    className="hover:text-blue-600"
+                  >
+                    Change Password
+                  </Link>
+                  <Link
+                    to="/employee/forget-password"
+                    onClick={backHandler}
+                    className="hover:text-blue-600"
+                  >
+                    Forget Password
+                  </Link>
+                  <Link onClick={LogoutHandler} className="hover:text-blue-600">
+                    Logout
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </header>
     );
 };
 
