6671588a525ddfa3f1d7ffb0804d6bd9d93bdc0d
Tarun Koshti <tarunkoshti910@gmail.com>
2024-05-12 12:04:44 +0530

    mongodb

--- Backend/models/db.js
+++ Backend/models/db.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose")
 
 exports.connectDatabase = async () => {
      try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(DB);
         console.log("database connected successfully");
      } catch (error) {
-        console.log("error.message")
+        console.log(error.message)
      }
 }
