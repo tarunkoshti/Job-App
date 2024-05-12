9acdbca001556ef1927d018dba0d91d16ce4d9d4
Tarun Koshti <tarunkoshti910@gmail.com>
2024-05-12 22:14:53 +0530

    vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       '/api': {
         target: 'https://job-app-api-gamma.vercel.app',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api\/?/, ''),
       },
     }
   },
