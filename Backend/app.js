20110f8f30e816ddca9c06930be24fbb77f4f30c
Tarun Koshti <tarunkoshti910@gmail.com>
2024-06-10 15:53:01 +0530

    chagne in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 app.use(
     cors({
-        origin: "http://localhost:5173/",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
         credentials: true,
     })
