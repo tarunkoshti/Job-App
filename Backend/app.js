c5220ecc1531c36ae64e84dfe266f8b43ba4b56c
Tarun Koshti <tarunkoshti910@gmail.com>
2024-06-10 15:46:05 +0530

    change origin to localhost

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 app.use(
     cors({
-        origin: "https://job-app-six-iota.vercel.app",
+        origin: "http://localhost:5173/",
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
         credentials: true,
     })
