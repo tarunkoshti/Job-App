dde4ed0913380acf22e64edd35203e66f9adb54a
Tarun Koshti <tarunkoshti910@gmail.com>
2024-06-10 15:51:16 +0530

    chnage backend to 3000

--- Frontend/src/config/axios.jsx
+++ Frontend/src/config/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://job-app-api-gamma.vercel.app",
+    baseURL: "http://localhost:3000",
     withCredentials: true,
 });
 
